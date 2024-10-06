8e80e788f8b0753f62562881803763930380c603
Rolando Murillo <rolandomurillo@hey.com>
2024-10-06 12:57:26 -0400

    Update images

--- modules/Portfolio/items.ts
+++ modules/Portfolio/items.ts
@@ -12,14 +12,14 @@ My friend Diego and I made a Figma plugin to upload designs as you work on them 
 
 It was really neat, we [launched it](https://www.producthunt.com/products/sensive#sensive) on Product Hunt to positive reception. We even got a few paying customers! 
     `,
-    src: "https://firebasestorage.googleapis.com/v0/b/blocks-e243f.appspot.com/o/b605f3c8-1a69-4c63-af5b-06d09f357561%2Fimage.png?alt=media&token=b7a454bd-e865-4b03-bf7c-9a54448534dc",
+    src: "/portfolio/sensive.png",
   },
   {
     title: "Titan",
     description: `
 I joined Titan in 2022 to help build a new kind of wealth management platform. I’ve never grown as a product-maker in such a short period of time. 
     `,
-    src: "https://framerusercontent.com/images/RUcrvFEqsnbH9ZBYtwc8r8YV8w.png?scale-down-to=2048",
+    src: "/portfolio/titan.png",
   },
   {
     title: "Bleep",
@@ -28,13 +28,13 @@ The note-taking / inspiration gathering / life organizing tool I’ve been makin
 
 But also, [check it out](https://bleep.is). It's pretty cool.
 `,
-    src: "https://framerusercontent.com/images/oNt0ESKQfWYVWLc5FVTcPZwjM8.png",
+    src: "/portfolio/bleep.png",
   },
   {
     title: "Air",
     description: `
 Thanks to Sensive, I met the wonderful team over at [Air](https://www.air.inc). Together, we built a really solid media management app for creative teams.`,
-    src: "https://img.plasmic.app/img-optimizer/v1/img/7358fcb53e0c6829ec0bfc2a1ff86ff6.png?q=75&f=webp",
+    src: "/portfolio/air.png",
   },
   {
     title: "Coleure",
@@ -43,6 +43,6 @@ Choosing colors is a fun part of the design process. But it often can be intimid
 
 A few years after launching, it got purchased by a media company for internal use, and thus sadly taken down from the public.
     `,
-    src: "https://framerusercontent.com/images/KmUXgRvK2EBCZ4on5tm20HS6Ns.png",
+    src: "/portfolio/coleure.png",
   },
 ];
