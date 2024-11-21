6bb03840b09f2984d7a8b3da3e18a3a1021e9e48
Rolando Murillo <rolandomurillo@hey.com>
2024-11-20 20:51:53 -0500

    Add Google Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-0HVRQ30KRH`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0HVRQ30KRH');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
