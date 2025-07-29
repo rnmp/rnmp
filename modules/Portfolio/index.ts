fe99ce463fbf9cdaa8fd531afbdd3a8ab164654b
Rolando Murillo <rolandomurillo@hey.com>
2025-07-28 22:08:43 -0400

    Bye bye react

--- modules/Portfolio/index.ts
+++
@@ -1,3 +0,0 @@
-export { PortfolioItemWidget } from "./components/PortfolioItemWidget";
-export { PortfolioItems } from "./items";
-export type { PortfolioItem } from "./items";
--- src/modules/Portfolio/index.ts
+++
@@ -1,2 +0,0 @@
-export { PortfolioItemWidget } from "./components/PortfolioItemWidget";
-export { PortfolioItems, type PortfolioItem } from "./items";
